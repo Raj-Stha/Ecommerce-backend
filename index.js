4ff8ae208dd024dfea400ef654f01cc7af096527
Raj Stha <rajcrestha00@gmail.com>
2023-05-15 09:53:24 +0545

    Added the cors to fix cross site origin

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+
+const cors = require('cors');
 const userRoute = require('./route/userRoute');
 
 mongoose.connect('mongodb+srv://rajstha:9840rajstha@cluster0.lvr5vhq.mongodb.net/')
@@ -10,7 +12,7 @@ mongoose.connect('mongodb+srv://rajstha:9840rajstha@cluster0.lvr5vhq.mongodb.net
   })
   .catch((err) => { console.log(err); })
 
-
+app.use(cors());
 app.use(express.json());
 
 
